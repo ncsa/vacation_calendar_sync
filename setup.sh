4f025fa3e3c99b79220fa3c5503bcca25215ba17
Phong <phongttran01@gmail.com>
2023-06-06 16:39:20 -0500

    Implement docker pull and run to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 REGISTRY=ghcr.io
 REPO=ncsa/vacation_calendar_sync
+BRANCH=main
 
+# Pulls the latest image from main branch
+docker pull $REGISTRY/$REPO:$BRANCH
 
-# Change it so that it pulls latest from main 
-docker pull $REGISTRY/$REPO:main
-#docker pull ghcr.io/ncsa/vacation_calendar_sync:1685990588_logging_feature_54634f5
-
-#docker run -it --name ncsa_vacation_calendar_sync --mount type=bind,source=$HOME,dst=/home $REGISTRY/$REPO
+# Creates a docker container
+docker run -it --name ncsa_vacation_calendar_sync --mount type=bind,source=$HOME,dst=/home $REGISTRY/$REPO:$BRANCH
