f461189e4298254ea93311eed999dabaef421e2c
Phong <phongttran01@gmail.com>
2023-07-27 12:37:37 -0500

    update

---
+++ go.sh
@@ -0,0 +1,21 @@
+DEBUG=1
+REGISTRY=ghcr.io
+REPO=ncsa/vacation_calendar_sync
+tag=main
+
+# function latest_tag {
+#   [[ "$DEBUG" -eq 1 ]] && set -x
+#   echo "production"
+# }
+
+#[[ "$DEBUG" -eq 1 ]] && set -x
+
+#tag=$(latest_tag)
+
+docker run -it --pull always \
+--mount type=bind,src=$HOME,dst=/home \
+$REGISTRY/$REPO:$tag 
+
+
+
+
