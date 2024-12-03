7bc116d0089c0780eabe4c28f6d947928f2dc4e9
Phong Tran <phongttran01@gmail.com>
2024-12-03 12:11:05 -0600

    Issue 22: Removed parameter checking

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,2 @@
 #!/bin/bash
-CONFIG_FILE=/root/vacation_calendar_sync_config.yaml
-REQUIRED_PARAMETERS="client_id tenant_id recipient_email scopes group_name shared_calendar_name category_name category_color vcs_directory AM_config PM config days_out update_interval email_list_update_interval"
-
-if [[ -f "$CONFIG_FILE" ]]; then  
-  for i in $REQUIRED_PARAMETERS; do 
-    result=$(grep -q $i $CONFIG_FILE; echo $?)
-    if [ "$result" -ne 0 ]; then
-      echo $i is missing
-      exit 1
-    fi
-  done
-  python3 OutlookCalendar.py -s
-else
-  echo Need config file named vacation_calendar_sync_config at your local home directory
-fi  
+python3 OutlookCalendar.py -s
